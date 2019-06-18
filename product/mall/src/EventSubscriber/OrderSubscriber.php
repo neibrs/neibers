b0702fe54d7c4c10739214f41cc2ffdc877f882a
John.nie <John-lee@2001594.no-reply.drupal.org>
2019-06-18 14:38:36 +0800

    Fix event subscribe.

---
+++ product/mall/src/EventSubscriber/OrderSubscriber.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\neibers_mall\EventSubscriber;
+
+use Drupal\commerce_recurring\EventSubscriber\OrderSubscriber as OrderSubscriberBase;
+
+class OrderSubscriber extends OrderSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events['neibers_mall.place.pre_transition'] = 'onPlace';
+    $events['neibers_mall.cancel.pre_transition'] = 'onCancel';
+    return $events;
+  }
+
+}
