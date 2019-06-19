4ddbffd16fcd5688dc9c100bb7b893700f09084c
John.nie <John-lee@2001594.no-reply.drupal.org>
2019-06-19 17:58:48 +0800

    fix.

--- bin/export_translations.sh
+++ bin/export_translations.sh
@@ -2,10 +2,6 @@
 
 vendor/bin/drupal moi potx -y
 
-# print the directory and file.
-#cd /var/www/html
-echo $(pwd)
-
 for file in `find modules/neibers -name "*.info.yml"`; do
   echo $(dirname $file) -- $(basename $(dirname $file));
   $(pwd)/vendor/bin/drush potx single --include=modules/contrib/potx --folder="$(dirname $file)/" --api=8
