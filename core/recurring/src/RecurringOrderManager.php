c31acb5fe8adcc49f585a0d2ea14c254b8f67605
John.nie <John-lee@2001594.no-reply.drupal.org>
2019-06-27 09:55:17 +0800

    Alter recurring service.

---
+++ core/recurring/src/RecurringOrderManager.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\neibers_recurring;
+
+use Drupal\commerce_recurring\RecurringOrderManager as RecurringOrderManagerBase;
+
+class RecurringOrderManager extends RecurringOrderManagerBase {
+
+}
---
+++ core/recurring/src/RecurringServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\neibers_recurring;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceModifierInterface;
+
+class RecurringServiceProvider implements ServiceModifierInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(ContainerBuilder $container) {
+    $definition = $container->getDefinition('commerce_recurring.order_manager');
+    $definition->setClass('Drupal\neibers_recurring\RecurringOrderManagerBase');
+  }
+}
