182d5f88cf8cb7d07ecc95786b16313b0bc25f9e
John.nie <John-lee@2001594.no-reply.drupal.org>
2019-06-27 10:18:37 +0800

    Add recurring event subscriber

--- core/recurring/src/EventSubscriber/OrderSubscriber.php
+++ core/recurring/src/EventSubscriber/OrderSubscriber.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\neibers_mall\EventSubscriber;
+namespace Drupal\neibers_recurring\EventSubscriber;
 
 use Drupal\commerce_recurring\EventSubscriber\OrderSubscriber as OrderSubscriberBase;
 
@@ -10,7 +10,6 @@ class OrderSubscriber extends OrderSubscriberBase {
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
-    // TODO neibers_recurring is not depend on mall module.
     $events['neibers_mall.place.pre_transition'] = 'onPlace';
     $events['neibers_mall.cancel.pre_transition'] = 'onCancel';
     return $events;
