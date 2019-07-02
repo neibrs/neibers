79a71726f3f2b31387d09491d557794c641b6e1c
John.nie <John-lee@2001594.no-reply.drupal.org>
2019-07-02 13:46:29 +0800

    Remove themes basestrap and exsen.

--- themes/basestrap/assets/js/global.js
+++
@@ -1,7 +0,0 @@
-(function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.basestrap = {
-    attach: function attach(context) {
-      console.log('basestrap');
-    }
-  };
-})(jQuery, Drupal, drupalSettings);
