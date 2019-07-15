61c15bb6618f49707b1cc22115ce5dfe0043619f
John.nie <John-lee@2001594.no-reply.drupal.org>
2019-07-15 22:13:04 +0800

    Fix all category structure.

--- themes/codex/js/mall.js
+++ themes/codex/js/mall.js
@@ -8,6 +8,44 @@
         $(this).removeClass("hover");
       });
 
+      //Expanded all the nav category
+      $("*[ectype='cateItem']").once('cateItem').on('mouseenter',function(){
+        var T = $(this),
+          cat_id = T.data('id'),
+          eveval = T.data('eveval'),
+          layer = T.find("*[ectype='cateLayer']");
+
+        if(eveval != 1){
+          T.data('eveval', '1');
+          /*加载中*/
+          // layer.find("*[ectype='cateLayerCon_" + cat_id + "']").html('<img src="'+ settings.theme.path +'/images/load/loadGoods.gif" width="200" height="200" class="lazy">');
+        //   $.ajax({
+        //     type: "GET",
+        //     url: "ajax_dialog.php",
+        //     data: "act=getCategoryCallback&cat_id=" + cat_id,
+        //     dataType:'json',
+        //     success: function(data){
+        //       if(data.has_child == 0){
+        //         T.find("*[ectype='cateLayer']").addClass("cateLayer_items_all");
+        //       }else{
+        //         T.find("*[ectype='cateLayer']").removeClass("cateLayer_items_all");
+        //       }
+        //
+        //       var channels = $("*[ectype='cateLayerCon_" + data.cat_id + "']");
+        //       channels.html(data.cat_content);
+        //     }
+        //   });
+        }
+
+        T.addClass("selected");
+        layer.show();
+      }).on("mouseleave",function(){
+        var T = $(this),layer = T.parent().find("*[ectype='cateLayer']");
+        T.removeClass("selected");
+        layer.hide();
+      });
+
+
     }
   };
 })(jQuery, Drupal, drupalSettings);
