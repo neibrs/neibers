94bf919d2bb873b983971619c376920d8fa0f34f
John.nie <John-lee@2001594.no-reply.drupal.org>
2019-07-27 15:56:09 +0800

    delete profile neibrs and profile base on varbase profile.

--- bin/install.sh
+++ bin/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #rm sites/default/settings.php
 
-#vendor/bin/drush site:install -y --account-pass=admin --db-url=mysql://root:root@mariadb/neibrs
-vendor/bin/drush site:install -y --account-pass=admin --db-url=mysql://root:root@127.0.0.1/neibers
+#bin/drush site:install -y --account-pass=admin --db-url=mysql://root:root@mariadb/neibrs
+bin/drush site:install -y --account-pass=admin --db-url=mysql://root:root@127.0.0.1/neibers
 
-vendor/bin/drupal site:mode dev
+bin/drupal site:mode dev
 
-vendor/bin/drush pmu -y toolbar
-vendor/bin/drush en -y coffee \
+bin/drush pmu -y toolbar
+bin/drush en -y coffee \
   config_update_ui \
   entity_plus \
   webprofiler \
@@ -21,17 +21,17 @@ vendor/bin/drush en -y coffee \
 echo "include $app_root . '/' . $site_path . '/settings.local.php';" > sites/default/settings.php
 
 # Initial demo data.
-vendor/bin/drush mim ip_xls
-#vendor/bin/drush mim product_xls
+bin/drush mim ip_xls
+#bin/drush mim product_xls
 
-vendor/bin/drupal locale:language:add zh-hans
+bin/drupal locale:language:add zh-hans
 
-vendor/bin/drush cset -y language.negotiation url.prefixes.en "en"
-vendor/bin/drush cset -y language.types negotiation.language_interface.enabled.language-browser 0
+bin/drush cset -y language.negotiation url.prefixes.en "en"
+bin/drush cset -y language.types negotiation.language_interface.enabled.language-browser 0
 
-#vendor/bin/drush cset -y system.site default_langcode "zh-hans"
-vendor/bin/drush locale:update
+#bin/drush cset -y system.site default_langcode "zh-hans"
+bin/drush locale:update
 
-vendor/bin/drupal thi -y neibers_codex
-vendor/bin/drush cset -y system.theme admin neibers_codex
-vendor/bin/drush cset -y system.theme default neibers_codex
+bin/drupal thi -y neibers_codex
+bin/drush cset -y system.theme admin neibers_codex
+bin/drush cset -y system.theme default neibers_codex
--- profiles/neibrs/README.md
+++
@@ -1,3 +0,0 @@
-The config optional ymls are conflict from profile standard optional.
-
-The default profile for neibers that is used for construct idc platform.
