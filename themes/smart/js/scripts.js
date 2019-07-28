0709070988aeb48a965fb52fd03df85df32312c8
John.nie <John-lee@2001594.no-reply.drupal.org>
2019-07-28 21:29:26 +0800

    Add smart theme base on vartheme-bs4.

---
+++ themes/smart/js/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Behaviors for the smart theme.
+ */
+
+(function ($, _, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.smart = {
+    attach: function (context) {
+
+      // smart JavaScript behaviors goes here.
+    }
+  };
+
+})(window.jQuery, window._, window.Drupal, window.drupalSettings);
